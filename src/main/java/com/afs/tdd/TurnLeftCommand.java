059db4244371f61b97a7b9b794e52abf6243a69e
Tom Lo <loto2@oocl.com>
2020-11-26 22:15:52 +0800

    [mars-rover][Tom] feat: implement TurnLeftCommand

--- src/main/java/com/afs/tdd/TurnLeftCommand.java
+++ src/main/java/com/afs/tdd/TurnLeftCommand.java
@@ -3,5 +3,21 @@ package com.afs.tdd;
 public class TurnLeftCommand implements RoverCommand {
     @Override
     public void execute(MarsRover rover) {
+        switch (rover.getDirection()) {
+            case MarsRover.NORTH:
+                rover.setDirection(MarsRover.WEST);
+                break;
+            case MarsRover.WEST:
+                rover.setDirection(MarsRover.SOUTH);
+                break;
+            case MarsRover.SOUTH:
+                rover.setDirection(MarsRover.EAST);
+                break;
+            case MarsRover.EAST:
+                rover.setDirection(MarsRover.NORTH);
+                break;
+            default:
+                break;
+        }
     }
 }
